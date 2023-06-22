686383599997c10c59727728ddc10733b9f93c12
Alex Sid <azzlem@mail.ru>
2023-06-22 19:02:15 +0300

    final realize

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,29 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def notebook():
+    return Item("notebook", 1000, 50)
+
+
+def test_init_(notebook):
+    assert notebook.name == "notebook"
+    assert notebook.price == 1000
+    assert notebook.quantity == 50
+    assert len(notebook.all) == 1
+
+
+def test_calculate_total_price(notebook):
+    assert notebook.calculate_total_price() == 50000
+
+
+
+Item.pay_rate = 0.8
+
+
+def test_apply_discount(notebook):
+    notebook.apply_discount()
+    assert notebook.price == 800
